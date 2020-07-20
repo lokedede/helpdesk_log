1d30429da7b39515ea11e07b3e0d64c6781e6812
Aiqi Zhang <aiqi.zhang@tusimple.ai>
2020-07-21 00:43:27 +0800

    init

---
+++ bash_version/log_scrape.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# extract log from gz file
+gunzip ./Helpdesk_interview_data_set.gz
+# use logstash docker to get required fields and generate json. logstash config details can be found in helpdesk_log_pipeline.conf
+docker run --detach --rm -v $(pwd):/logstash_tmp docker.elastic.co/logstash/logstash:7.8.0 /usr/share/logstash/bin/logstash -f /logstash_tmp/helpdesk_log_pipeline.conf
+echo "output json is generated in  $(pwd)/output.json"
+sleep 60
+# post json to server
+curl -X POST -H "Content-Type: application/json" -d @./output.json https://foo.com/bar
